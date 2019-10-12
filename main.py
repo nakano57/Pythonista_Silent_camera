49f25774e589a6088c7aabaeedecdc9a20b0be4f
nakano57 <hiroto856@gmail.com>
2019-10-12 16:04:47 +0900

    minor change

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import io
 import os
 import time
 import photos
-import coreimage
 import tempfile
+import coreimage
 import concurrent.futures
 import webbrowser
 
@@ -35,227 +35,204 @@ UIImage = ObjCClass('UIImage')
 
 dispatch_get_current_queue = c.dispatch_get_current_queue
 dispatch_get_current_queue.restype = c_void_p
-		
-class muon():
-	def __init__(self):
-		self.last_fps_time = time.time()
-		self.fps_counter =0
-		self.ciimage = None
-		self.take_photo_flag = False
-		self.captureFlag = False
-		self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
-		self.whiteWaiter = concurrent.futures.ThreadPoolExecutor(max_workers=2)
-		
-		self.mainView = ui.View()
-		self.mainView.background_color = 'black'
-		self.mainView.height = ui.get_screen_size()[0]*1.78
-		self.mainView.width = ui.get_screen_size()[0]
-		self.mainView.name = 'Silent Camera'
-		
-		#if iPad == True: #overwrite
-			#self.height = ui.get_screen_size()[1]/1.3
-			#self.width = ui.get_screen_size()[0]/1.5
-			
-		sampleBufferDelegate = create_objc_class(
-		'sampleBufferDelegate',
-		methods=[self.captureOutput_didOutputSampleBuffer_fromConnection_],
-		protocols=['AVCaptureVideoDataOutputSampleBufferDelegate'])
-		delegate = sampleBufferDelegate.new()
-		
-		session = AVCaptureSession.alloc().init()
-		device = AVCaptureDevice.defaultDeviceWithMediaType_('vide')
-		_input = AVCaptureDeviceInput.deviceInputWithDevice_error_(device, None)
-		if _input:
-			session.addInput_(_input)
-		else:
-			print('Failed to create input')
-			return
-			
-		output = AVCaptureVideoDataOutput.alloc().init()
-		queue = ObjCInstance(dispatch_get_current_queue())
-		output.setSampleBufferDelegate_queue_(delegate, queue)
-		output.alwaysDiscardsLateVideoFrames = True
-		
-		session.addOutput_(output)
-		session.sessionPreset = 'AVCaptureSessionPreset3840x2160'
-		
-		prev_layer = AVCaptureVideoPreviewLayer.layerWithSession_(session)
-		prev_layer.frame = ObjCInstance(self.mainView).bounds()
-		prev_layer.setVideoGravity_('AVLayerVideoGravityResizeAspectFill')
-		
-		ObjCInstance(self.mainView).layer().addSublayer_(prev_layer)
-		
-		self.whitenView = ui.View()
-		self.whitenView.height = self.mainView.height
-		self.whitenView.width = self.mainView.width
-		self.whitenView.background_color = 'white'
-		self.whitenView.alpha = 0.0
-	
-		button = ui.Button()
-		button.flex = 'T'
-		button.width = ui.get_screen_size()[0]*0.24
-		button.height = button.width
-		button.center = (self.mainView.width*0.5,self.mainView.height*0.85)
-		button.action = self.button_tapped
-		button.background_image = ui.Image('iow:ios7_circle_filled_256')
-		#button.alpha=0.8
-		
-		self.latestPhotoView = ui.ImageView()
-		self.latestPhotoView.background_color = 'white'
-		self.latestPhotoView.flex='T'
-		self.latestPhotoView.height = 50
-		self.latestPhotoView.width = self.latestPhotoView.height
-		self.latestPhotoView.center = (self.mainView.width*0.12,self.mainView.height*0.85)
-		self.latestPhotoView.image = self.get_latest_photo()
-		
-		self.savingPhotoView = ui.ImageView()
-		self.savingPhotoView.background_color = 'black'
-		self.savingPhotoView.flex='T'
-		self.savingPhotoView.height = 50
-		self.savingPhotoView.width = self.savingPhotoView.height
-		self.savingPhotoView.center = (self.mainView.width*0.12,self.mainView.height*0.85)
-		self.savingPhotoView.image = ui.Image('iow:load_d_32')
-		self.savingPhotoView.alpha=0.0
-		
-		openPhotoapp = ui.Button()
-		openPhotoapp.flex='T'
-		openPhotoapp.height = 50
-		openPhotoapp.width = openPhotoapp.height
-		openPhotoapp.center = (self.mainView.width*0.12,self.mainView.height*0.85)
-		openPhotoapp.action = self._openPhotoapp
-	
-		self.mainView.add_subview(self.whitenView)
-		self.mainView.add_subview(button)
-		self.mainView.add_subview(self.latestPhotoView)
-		self.mainView.add_subview(self.savingPhotoView)
-		self.mainView.add_subview(openPhotoapp)
-		
-		session.startRunning()
-		
-		self.mainView.present('fullscreen',title_bar_color='black')
-		self.mainView.wait_modal()
-		
-		session.stopRunning()
-		delegate.release()
-		session.release()
-		output.release()
-		
-		
-	def _openPhotoapp(self,sender):
-		self.mainView.close()
-		webbrowser.open('photos-redirect://')
-		exit()
-	
-	def button_tapped(self,sender):
-		self.whitenView.alpha = 1.0
-		self.take_photo_flag = True
-		#self.take_photo()
-		self.executor.submit(self.take_photo)
-		
-	def _whiteWaiter(self):
-		for i in range(10):
-			time.sleep(0.008)
-			if i == 0:
-				self.whitenView.alpha = 9.0
-			else:
-				self.whitenView.alpha = (9-i)/10
-		
-	def take_photo(self):	
-		while True:
-			if self.captureFlag == True:
-				self.captureFlag = False
-				break
-				
-		self.whiteWaiter.submit(self._whiteWaiter)
-		self.savingPhotoView.alpha=0.5
-		self.img = coreimage.CImage()
-		self.img.ci_img = self.ciimage
-		
-		
-		ui_img = self.img.get_uiImg()
-		
-		pilimg = self.img.get_PIL()
-		pilimg = pilimg.rotate(270, expand=True)
-		self.pilSaveAlbulm(pilimg)
-		self.latestPhotoView.image = self.get_latest_photo()
-		self.savingPhotoView.alpha=0.0
-		#self.stopView.image = None
-		#self.img.save_album()
-	
-	def pilSaveAlbulm(self,pilImg):
-		#temp_path = os.path.join(tempfile.gettempdir(), 'piltemp.png')
-		pilImg.save('temp.png')
-		photos.create_image_asset('temp.png')
 
-	def get_latest_photo(self):
-		all_assets = photos.get_assets()
-		last_asset = all_assets[-1]
-		img = last_asset.get_image()
-		
-		if img.width >= img.height:	
-			img = img.resize((100,round(img.height/img.width*100)))
-		else:
-			img = img.resize((round(img.width/img.height*100),100))
-			
-		return self.pil2ui(img)
-		
-	def openPIL(self,fileName='output.jpg'):
-		print('[LOG]Opening...')
-		pilImg = Image.open('output.jpg')
-		print('[LOG]Opened')
-		print('[LOG]Showing...')
-		pilImg.show()
-		print('[LOG]Showed')
-	
-	def pil2ui(self,imgIn):
-		with io.BytesIO() as bIO:
-			imgIn.save(bIO, 'PNG')
-			imgOut = ui.Image.from_data(bIO.getvalue())
-		del bIO
-		return imgOut
-		
-	def ci2ui(self,ci_img):
-		ctx = ObjCClass('CIContext').context()
-		extent = ci_img.extent()
-		m = ctx.outputImageMaximumSize()
-		if extent.size.width > m.width or extent.size.height > m.height:
-			extent = CGRect(CGPoint(0, 0), CGSize(1024, 1024))
-		cg_img = ctx.createCGImage_fromRect_(ci_img, extent)
-		ui_img = UIImage.imageWithCGImage_(cg_img)
-		c.CGImageRelease.argtypes = [c_void_p]
-		c.CGImageRelease.restype = None
-		c.CGImageRelease(cg_img)
-		return ui_img
-		
-	def captureOutput_didOutputSampleBuffer_fromConnection_(self, _self, _cmd, _output, _sample_buffer, *args):
-		#print(args)
-		# 性能確認のためビデオデータの実 FPS 表示
-		#self.fps_counter += 1
-		#now = time.time()
-		#self.buf = _sample_buffer
-		#if int(now) > int(self.last_fps_time):
-			#self.label_fps.text = '{:5.2f} fps'.format((self.fps_counter) / (now - self.last_fps_time))
-			#self.last_fps_time = now
-			#self.fps_counter = 0
-			
-		# 画像処理は FRAME_INTERVAL 間隔で処理
-	
-		# ビデオ画像のフレームデータを取得
-		if self.take_photo_flag == True:
-			self.take_photo_flag = False
-			imagebuffer =  CMSampleBufferGetImageBuffer(_sample_buffer)	
-			
-			# バッファをロック
-			CVPixelBufferLockBaseAddress(imagebuffer, 0)
-			self.ciimage = CIImage.imageWithCVPixelBuffer_(ObjCInstance(imagebuffer))
-			
-			#print(self.ciimage)
-			# バッファのロックを解放
-			CVPixelBufferUnlockBaseAddress(imagebuffer, 0)
-			self.captureFlag = True
-			
-		
-		
-		
+
+class muon():
+    def __init__(self):
+        self.ciimage = None
+        self.take_photo_flag = False
+        self.captureFlag = False
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+        self.whiteWaiter = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+
+        self.mainView = ui.View()
+        self.mainView.background_color = 'black'
+        self.mainView.height = ui.get_screen_size()[0]*1.78
+        self.mainView.width = ui.get_screen_size()[0]
+        self.mainView.name = 'Silent Camera'
+
+        # if iPad == True: #overwrite
+        #self.height = ui.get_screen_size()[1]/1.3
+        #self.width = ui.get_screen_size()[0]/1.5
+
+        sampleBufferDelegate = create_objc_class(
+            'sampleBufferDelegate',
+            methods=[
+                self.captureOutput_didOutputSampleBuffer_fromConnection_],
+            protocols=['AVCaptureVideoDataOutputSampleBufferDelegate'])
+        delegate = sampleBufferDelegate.new()
+
+        session = AVCaptureSession.alloc().init()
+        device = AVCaptureDevice.defaultDeviceWithMediaType_('vide')
+        _input = AVCaptureDeviceInput.deviceInputWithDevice_error_(
+            device, None)
+        if _input:
+            session.addInput_(_input)
+        else:
+            print('Failed to create input')
+            return
+
+        output = AVCaptureVideoDataOutput.alloc().init()
+        queue = ObjCInstance(dispatch_get_current_queue())
+        output.setSampleBufferDelegate_queue_(delegate, queue)
+        output.alwaysDiscardsLateVideoFrames = True
+
+        session.addOutput_(output)
+        session.sessionPreset = 'AVCaptureSessionPreset3840x2160'
+
+        prev_layer = AVCaptureVideoPreviewLayer.layerWithSession_(session)
+        prev_layer.frame = ObjCInstance(self.mainView).bounds()
+        prev_layer.setVideoGravity_('AVLayerVideoGravityResizeAspectFill')
+
+        ObjCInstance(self.mainView).layer().addSublayer_(prev_layer)
+
+        self.whitenView = ui.View()
+        self.whitenView.height = self.mainView.height
+        self.whitenView.width = self.mainView.width
+        self.whitenView.background_color = 'white'
+        self.whitenView.alpha = 0.0
+
+        button = ui.Button()
+        button.flex = 'T'
+        button.width = ui.get_screen_size()[0]*0.24
+        button.height = button.width
+        button.center = (self.mainView.width*0.5, self.mainView.height*0.85)
+        button.action = self.button_tapped
+        button.background_image = ui.Image('iow:ios7_circle_filled_256')
+        # button.alpha=0.8
+
+        self.latestPhotoView = ui.ImageView()
+        self.latestPhotoView.background_color = 'white'
+        self.latestPhotoView.flex = 'T'
+        self.latestPhotoView.height = 50
+        self.latestPhotoView.width = self.latestPhotoView.height
+        self.latestPhotoView.center = (
+            self.mainView.width*0.12, self.mainView.height*0.85)
+        self.latestPhotoView.image = self.get_latest_photo()
+
+        self.savingPhotoView = ui.ImageView()
+        self.savingPhotoView.background_color = 'black'
+        self.savingPhotoView.flex = 'T'
+        self.savingPhotoView.height = 50
+        self.savingPhotoView.width = self.savingPhotoView.height
+        self.savingPhotoView.center = (
+            self.mainView.width*0.12, self.mainView.height*0.85)
+        self.savingPhotoView.image = ui.Image('iow:load_d_32')
+        self.savingPhotoView.alpha = 0.0
+
+        openPhotoapp = ui.Button()
+        openPhotoapp.flex = 'T'
+        openPhotoapp.height = 50
+        openPhotoapp.width = openPhotoapp.height
+        openPhotoapp.center = (self.mainView.width*0.12,
+                               self.mainView.height*0.85)
+        openPhotoapp.action = self._openPhotoapp
+
+        self.mainView.add_subview(self.whitenView)
+        self.mainView.add_subview(button)
+        self.mainView.add_subview(self.latestPhotoView)
+        self.mainView.add_subview(self.savingPhotoView)
+        self.mainView.add_subview(openPhotoapp)
+
+        session.startRunning()
+
+        self.mainView.present('fullscreen', title_bar_color='black')
+        self.mainView.wait_modal()
+
+        session.stopRunning()
+        delegate.release()
+        session.release()
+        output.release()
+
+    def _openPhotoapp(self, sender):
+        self.mainView.close()
+        webbrowser.open('photos-redirect://')
+        exit()
+
+    def _whiteWaiter(self):
+        for i in range(10):
+            time.sleep(0.008)
+            if i == 0:
+                self.whitenView.alpha = 9.0
+            else:
+                self.whitenView.alpha = (9-i)/10
+
+    def button_tapped(self, sender):
+        self.whitenView.alpha = 1.0
+        self.take_photo_flag = True
+        # self.take_photo()
+        self.executor.submit(self.take_photo)
+
+    def take_photo(self):
+        while True:
+            if self.captureFlag == True:
+                self.captureFlag = False
+                break
+
+        self.whiteWaiter.submit(self._whiteWaiter)
+        self.savingPhotoView.alpha = 0.5
+        self.img = coreimage.CImage()
+        self.img.ci_img = self.ciimage
+
+        #ui_img = self.img.get_uiImg()
+        pilimg = self.img.get_PIL()
+        pilimg = pilimg.rotate(270, expand=True)
+        self.pilSaveAlbulm(pilimg)
+        self.latestPhotoView.image = self.get_latest_photo()
+        self.savingPhotoView.alpha = 0.0
+        #self.stopView.image = None
+        # self.img.save_album()
+
+    def pilSaveAlbulm(self, pilImg):
+        #temp_path = os.path.join(tempfile.gettempdir(), 'temp.png')
+        pilImg.save('temp.png')
+        photos.create_image_asset('temp.png')
+
+    def pil2ui(self, imgIn):
+        with io.BytesIO() as bIO:
+            imgIn.save(bIO, 'PNG')
+            imgOut = ui.Image.from_data(bIO.getvalue())
+        del bIO
+        return imgOut
+
+    def ci2ui(self, ci_img):
+        ctx = ObjCClass('CIContext').context()
+        extent = ci_img.extent()
+        m = ctx.outputImageMaximumSize()
+        if extent.size.width > m.width or extent.size.height > m.height:
+            extent = CGRect(CGPoint(0, 0), CGSize(1024, 1024))
+        cg_img = ctx.createCGImage_fromRect_(ci_img, extent)
+        ui_img = UIImage.imageWithCGImage_(cg_img)
+        c.CGImageRelease.argtypes = [c_void_p]
+        c.CGImageRelease.restype = None
+        c.CGImageRelease(cg_img)
+        return ui_img
+
+    def get_latest_photo(self):
+        all_assets = photos.get_assets()
+        last_asset = all_assets[-1]
+        img = last_asset.get_image()
+
+        if img.width >= img.height:
+            img = img.resize((100, round(img.height/img.width*100)))
+        else:
+            img = img.resize((round(img.width/img.height*100), 100))
+
+        return self.pil2ui(img)
+
+    def captureOutput_didOutputSampleBuffer_fromConnection_(self, _self, _cmd, _output, _sample_buffer, *args):
+
+        if self.take_photo_flag == True:
+            self.take_photo_flag = False
+            imagebuffer = CMSampleBufferGetImageBuffer(_sample_buffer)
+
+            # バッファをロック
+            CVPixelBufferLockBaseAddress(imagebuffer, 0)
+            self.ciimage = CIImage.imageWithCVPixelBuffer_(
+                ObjCInstance(imagebuffer))
+            # バッファのロックを解放
+            CVPixelBufferUnlockBaseAddress(imagebuffer, 0)
+            self.captureFlag = True
+
+
 cam = muon()
-#cam.take_photo(0.0,'test.jpg')
